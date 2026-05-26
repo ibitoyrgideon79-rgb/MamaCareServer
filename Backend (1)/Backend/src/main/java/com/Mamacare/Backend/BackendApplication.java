e81d8c9c86865c6c1fc7e810c02b2504b3e671ca
Gideon Ibitoye <ibitoyrgideon79@gmail.com>
2026-05-26 10:49:30 +0100

    Delete sec test file

--- Backend (1)/Backend/src/main/java/com/Mamacare/Backend/BackendApplication.java
+++ Backend (1)/Backend/src/main/java/com/Mamacare/Backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.Mamacare.Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class BackendApplication {
 
 	public static void main(String[] args) {
